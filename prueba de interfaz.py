c145fa5a8cb5f301c6beb2d624405dfdf387013f
UnVato789 <88251332+UnVato789@users.noreply.github.com>
2022-11-07 16:17:25 -0700

    Update prueba de interfaz.py

--- prueba de interfaz.py
+++ prueba de interfaz.py
@@ -2,8 +2,8 @@ from tkinter import *
 import random
 
 root=Tk()
-root.title("SACATE LA LOTERIA")
-root.geometry("850x300")
+root.title("Generador de numeros random")
+root.geometry("850x400")
 
 random_number_list = Label(root)
 random_number_sorted_list = Label(root)
@@ -14,10 +14,25 @@ def randomlist():
     randomlist.sort()
     random_number_sorted_list["text"] = "Números aleatorios ordenados: " + str(randomlist)
 
-btn=Button(root,text="generate random list ",command=randomlist)
-btn.place(relx=0.5,rely=0.4,anchor=CENTER)
+def salir():
+    root.destroy()
+
+def eliminar():
+    randomlist = random.sample(range(0,0),0)
+    random_number_list["text"] = ""
+    randomlist.sort()
+    random_number_sorted_list["text"] = ""
+
+btn=Button(root,text="Generar lista",command=randomlist)
+btn.place(relx=0.5,rely=0.2,anchor=CENTER)
+
+btn=Button(root,text="Eliminar lista",command=eliminar)
+btn.place(relx=0.5,rely=0.3,anchor=CENTER)
 
 random_number_list.place(relx=0.5,rely=0.5,anchor=CENTER)
 random_number_sorted_list.place(relx=0.5,rely=0.6,anchor=CENTER)
+
+btn=Button(root,text="SALIR",command=salir)
+btn.place(relx=0.5,rely=0.8,anchor=CENTER)
 
 root.mainloop()
